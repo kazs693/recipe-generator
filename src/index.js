af796774d48976c6e2032b5a2509385e839201c7
kazs693 <karenesearle@gmail.com>
2025-11-08 14:19:08 +1100

    Added API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings: "Vegan spaghetti bolognese",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "233d7725d7of806acc300f5t46cfc23b";
+  let prompt = `Generate a ${instructionsInput.value} recipe`;
+  let context =
+    "You are an accomplished chef and love to share your unique and creative recipes. Your mission is to create a simple recipe using everyday ingredients following the dietary requirements provided. Please respond in HTML format and do not include markdown in your response.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating recipe");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
